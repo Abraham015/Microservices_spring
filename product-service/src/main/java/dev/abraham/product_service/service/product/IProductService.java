e8f329c23db861453b612cb950dcb6cbe6db11ff
AbrahamH015 <79383720+Abraham015@users.noreply.github.com>
2024-09-17 12:17:55 -0600

    Delete product-service directory

--- product-service/src/main/java/dev/abraham/product_service/service/product/IProductService.java
+++
@@ -1,8 +0,0 @@
-package dev.abraham.product_service.service.product;
-
-import dev.abraham.product_service.model.Product;
-import dev.abraham.product_service.request.ProductRequest;
-
-public interface IProductService {
-    Product addProduct(ProductRequest product);
-}
